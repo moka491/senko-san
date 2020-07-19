e269e5b5066116e0236a759ddd0da6bb2f67d7f3
Jannis Becker <jbecker@jannisbecker.me>
2020-07-19 14:56:01 +0200

    Fix command folder structure

--- src/commands/system/index.ts
+++ src/commands/system/index.ts
@@ -1,6 +1,6 @@
 import { PingCommand } from "./ping";
 import { CommandGroup } from "../../core/commands/CommandGroup";
 
-export const SystemGroup = new CommandGroup({ groupPrefix: "network" }, [
+export const SystemGroup = new CommandGroup({ groupPrefix: "system" }, [
   new PingCommand(),
 ]);
