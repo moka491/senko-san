d416069b7ab692fc62125a979aec6095b386bb93
Jannis Becker <jbecker@jannisbecker.me>
2020-07-31 16:01:02 +0200

    Add cron package and scheduler class

---
+++ src/core/Scheduler.ts
@@ -0,0 +1,27 @@
+import { CronJob } from "cron";
+import { ScheduleJob } from "./models/scheduler/ScheduleJob";
+
+export class Scheduler {
+  private jobList: Map<ScheduleJob, CronJob>;
+
+  constructor() {
+    this.jobList = new Map();
+  }
+
+  schedule(job: ScheduleJob) {
+    const cronJob = new CronJob(
+      job.cronRule || job.remindDate,
+      this.executeJob(job),
+      null,
+      true
+    );
+
+    this.jobList.set(job, cronJob);
+  }
+
+  scheduleAll(jobs: ScheduleJob[]) {
+    jobs.forEach(this.schedule);
+  }
+
+  private executeJob(job: ScheduleJob) {}
+}
