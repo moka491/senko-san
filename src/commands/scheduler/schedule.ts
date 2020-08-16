795c57443b2348953198d46756ea8c4db5f9ac18
Jannis Becker <jbecker@jannisbecker.me>
2020-08-16 11:22:00 +0200

    [WIP] Refactor schedule command to use new parsing

--- src/commands/scheduler/schedule.ts
+++ src/commands/scheduler/schedule.ts
@@ -5,16 +5,20 @@ import { command } from "../../core/commands/decorators";
 import { RegexConsumer } from "../../core/parsing/RegexConsumer";
 import { ScheduleJob } from "../../core/models/scheduler/ScheduleJob";
 import Cron from "../../core/parsing/Cron";
+import { DateTimeArgumentParser } from "../../core/parsing/scheduling/DateTimeArgumentParser";
 
 const END_CHARS = "(?= to| at| on| in| next| every| each| mentioning|$)";
 
 const RE_COMMAND = new RegExp('(to run)\\s+"(.*?)"' + END_CHARS);
 const RE_REMIND_MSG = new RegExp('(to)\\s+"(.*?)"' + END_CHARS);
+const RE_CHANNEL = new RegExp("(in channel)\\s+(.*?)" + END_CHARS);
 const RE_RECURRING_DATE_CMD = new RegExp("(every|each)\\s+(.*?)" + END_CHARS);
 const RE_FIXED_DATE_CMD = new RegExp("(on)\\s+(.*?)" + END_CHARS);
+const RE_THIS_DATE_CMD = new RegExp("(this)\\s+(.*?)" + END_CHARS);
 const RE_NEXT_DATE_CMD = new RegExp("(next)\\s+(.*?)" + END_CHARS);
+const RE_IN_DATE_CMD = new RegExp("(in)\\s+(.*?)" + END_CHARS);
 const RE_TIME = new RegExp("(at)\\s+(.*?)" + END_CHARS);
-const RE_CHANNEL = new RegExp("(in)\\s+(.*?)" + END_CHARS);
+
 const RE_MENTIONING_LIST = new RegExp("(mentioning)\\s+(.*?)" + END_CHARS);
 
 @command({
@@ -49,8 +53,6 @@ function processCommand(
 ) {
   // Define some flags to set during parsing
   let isRecurring: boolean = false;
-  let cronRule: Cron;
-  let fixedDate: Date;
   let channelId;
   let mentionUserIds;
 
@@ -59,157 +61,25 @@ function processCommand(
 
   // -schedule every Monday at 20:00 to run "say hello world" mentioning me and @User1
   // Parse all elements of the command
-  const reminderCommandArg = re.get(RE_COMMAND, 2); // say hello world
-  const reminderMessage = re.get(RE_REMIND_MSG, 2); // undefined
+  const [reminderCommandArg] = re.get(RE_COMMAND, [1, 2]); // say hello world
+  const [reminderMessage] = re.get(RE_REMIND_MSG, [2]); // undefined
 
-  const recurringUnitArg = re.get(RE_RECURRING_DATE_CMD, 2); // Monday
-  const nextUnitArg = re.get(RE_NEXT_DATE_CMD, 2); // undefined
-  const fixedDateArg = re.get(RE_FIXED_DATE_CMD, 2); // undefined
+  const [recurringUnitArg] = re.get(RE_RECURRING_DATE_CMD, [2]); // Monday
+  const [nextUnitArg] = re.get(RE_NEXT_DATE_CMD, [2]); // undefined
+  const [fixedDateArg] = re.get(RE_FIXED_DATE_CMD, [2]); // undefined
 
-  const timeArg = re.get(RE_TIME, 2); // 20:00
-  const channelArg = re.get(RE_CHANNEL, 2); // undefined
-  const mentioningUsersArg = re.get(RE_MENTIONING_LIST, 2); // me and @User1
+  const [timeArg] = re.get(RE_TIME, [2]); // 20:00
+  const [channelArg] = re.get(RE_CHANNEL, [2]); // undefined
+  const [mentioningUsersArg] = re.get(RE_MENTIONING_LIST, [2]); // me and @User1
 
   if (recurringUnitArg) {
-    // Create cron rule out of the available inputs, in this case the recurringUnitArg
-    // (can be weekday, or seconds/minutes/hours etc)
-    cronRule = {
-      ...cronRule,
-      ...parseWeekDayToCron(recurringUnitArg),
-      ...parseTimeUnitToCron(recurringUnitArg),
-      ...parseTimeToCron(timeArg),
-    };
+    const parsedWeekday = DateTimeArgumentParser.parseWeekday(recurringUnitArg);
+    const parsedDuration = DateTimeArgumentParser.parseTimeUnitArgument(
+      recurringUnitArg
+    );
 
-    // Set flag to not remove this job after the first run
-    isRecurring = true;
-
-    // parse time and overwrite cron numbers
-  } else if (nextUnitArg) {
-    // Create cron rule out of the available inputs, in this case the nextUnitArg
-    // (can be weekday, or seconds/minutes/hours etc)
-    cronRule = {
-      ...cronRule,
-      ...parseWeekDayToCron(nextUnitArg),
-      ...parseTimeUnitToCron(nextUnitArg),
-      ...parseTimeToCron(timeArg),
-    };
-  } else if (fixedDateArg) {
-    // try parsing the dateArg itself and set fixedDate
-    fixedDate = parseDateAndTime(fixedDateArg, timeArg);
+    console.log({ parsedWeekday, parsedDuration });
   }
-
-  channelId = parseChannelId(channelArg);
-  mentionUserIds = parseUserList(mentioningUsersArg, msg.author.id);
-
-  msg.reply(
-    JSON.stringify(
-      {
-        isRecurring,
-        cronRule,
-        fixedDate,
-        channelId,
-        mentionUserIds,
-      },
-      null,
-      4
-    )
-  );
-}
-
-function parseWeekDayToCron(input: string): Cron {
-  const index = [
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-    "sunday",
-  ].indexOf(input.toLowerCase());
-  return index > -1 ? { dayOfWeek: `${index}` } : {};
-}
-
-function parseTimeUnitToCron(input: string): Cron {
-  // Prepare input string
-  input = input.toLowerCase().trim();
-
-  // Remove number(s) from string and store it in step, or use 1 if none is given
-  const step = parseInt(input.replace(/^\D+/g, "")) || 1;
-  input = input.trim();
-
-  switch (input) {
-    case "second":
-    case "seconds":
-      return { second: `*/${step}` };
-    case "minute":
-    case "minutes":
-      return { second: "0", minute: `*/${step}` };
-    case "hour":
-    case "hours":
-      return { second: "0", minute: "0", hour: `*/${step}` };
-    case "day":
-    case "days":
-      return { second: "0", minute: "0", hour: "0", dayOfWeek: `*/${step}` };
-    case "month":
-    case "months":
-      return {
-        second: "0",
-        minute: "0",
-        hour: "0",
-        dayOfMonth: "1", // todo perhaps current day of month
-        month: `*/${step}`,
-        dayOfWeek: "*",
-      };
-    case "year":
-      return {
-        second: "0",
-        minute: "0",
-        hour: "0",
-        dayOfMonth: "1",
-        month: "1",
-        dayOfWeek: "*",
-      };
-  }
-
-  return {};
-}
-
-function parseTimeArg(input: string): [number, number] {
-  // Prepare input string
-  input = input.toLowerCase().trim();
-
-  const timeParts = input.split(":");
-
-  let hour = parseInt(timeParts[0]) || 0;
-  let minute = parseInt(timeParts[1]) || 0;
-
-  if (input.includes("pm")) {
-    hour += 12;
-  }
-
-  return [hour, minute];
-}
-
-function parseTimeToCron(input: string): Cron {
-  const [hour, minute] = parseTimeArg(input);
-  return { minute: `${minute}`, hour: `${hour}` };
-}
-
-function parseDateAndTime(dateArg: string, timeArg: string) {
-  dateArg = dateArg.toLowerCase().trim();
-
-  const parsedDate = new Date(dateArg);
-  if (parsedDate) {
-    if (timeArg) {
-      const [hour, minute] = parseTimeArg(timeArg);
-      parsedDate.setUTCHours(hour);
-      parsedDate.setUTCMinutes(minute);
-    }
-
-    return parsedDate;
-  }
-
-  return null;
 }
 
 function parseChannelId(input: string) {
