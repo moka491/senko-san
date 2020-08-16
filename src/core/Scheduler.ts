6e9899ab43feb36622611cd557c8fc72ee971d44
Jannis Becker <jbecker@jannisbecker.me>
2020-08-16 11:25:06 +0200

    [WIP] Rewrite Scheduler class for node-schedule

--- src/core/Scheduler.ts
+++ src/core/Scheduler.ts
@@ -1,26 +1,21 @@
-import { CronJob } from "cron";
-import { ScheduleJob } from "./models/scheduler/ScheduleJob";
+// import { ScheduleJob } from "./models/scheduler/ScheduleJob";
 
 export class Scheduler {
-  private jobList: Map<ScheduleJob, CronJob>;
-
-  constructor() {
-    this.jobList = new Map();
-  }
-
-  schedule(job: ScheduleJob) {
-    // const cronJob = new CronJob(
-    //   job.cronRule || job.fixedDate,
-    //   this.executeJob(job),
-    //   null,
-    //   true
-    // );
-    // this.jobList.set(job, cronJob);
-  }
-
-  scheduleAll(jobs: ScheduleJob[]) {
-    jobs.forEach(this.schedule);
-  }
-
-  private executeJob(job: ScheduleJob) {}
+  //   private jobList: Map<ScheduleJob, CronJob>;
+  //   constructor() {
+  //     this.jobList = new Map();
+  //   }
+  //   schedule(job: ScheduleJob) {
+  //     // const cronJob = new CronJob(
+  //     //   job.cronRule || job.fixedDate,
+  //     //   this.executeJob(job),
+  //     //   null,
+  //     //   true
+  //     // );
+  //     // this.jobList.set(job, cronJob);
+  //   }
+  //   scheduleAll(jobs: ScheduleJob[]) {
+  //     jobs.forEach(this.schedule);
+  //   }
+  //   private executeJob(job: ScheduleJob) {}
 }
